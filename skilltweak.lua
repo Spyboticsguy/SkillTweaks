f0565eae94519718fe46ecf275083a004df5b032
Tristen Allen <spyboticsguy@gmail.com>
2015-08-30 23:22:13 -0400

    Add code to modify Yakuza perk deck

--- skilltweak.lua
+++ skilltweak.lua
@@ -7,5 +7,7 @@ if RequiredScript == "lib/tweak_data/skilltreetweakdata" then
 	-- Add moving target skills to Cat Burglar
     table.insert(self.skills.cat_burglar[1].upgrades, "player_can_strafe_run")
 	table.insert(self.skills.cat_burglar[2].upgrades, "player_can_free_run")
+	-- Add infiltrator tier one skill to yakuza tier one
+	table.insert(self.specializations[12][1].upgrades, "player_damage_dampener_close_contact_1")
 	end
 end
